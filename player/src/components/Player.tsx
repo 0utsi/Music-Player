00c719d90ba36cbd1858953952c35c7d2b9603d8
“Mateusz” <mateusz.ludyk@gmail.com>
2023-02-05 17:00:03 +0100

    Działa bez contextu

--- player/src/components/Player.tsx
+++ player/src/components/Player.tsx
@@ -3,19 +3,39 @@ import { AlbumPic } from "./AlbumPic/AlbumPic";
 import { useState, useRef, useEffect } from "react";
 import "./Player.css";
 import { ProgresBar } from "./ProgresBar/ProgresBar";
+import typebeat from "../songs/typeBeat.mp3";
 
 export function Player(props: any) {
 	const [isPlaying, setIsPlaying] = useState(false);
+	const audioElement = useRef<HTMLAudioElement | null>();
+	const audioRef = useRef({
+		context: new AudioContext(),
+	});
+
+	// useEffect(() => {
+	// 	const mediaElement = audioRef.current.context.createMediaElementSource(
+	// 		audioElement.current
+	// 	);
+	// 	mediaElement.connect(audioRef.current.context.destination);
+	// }, []);
 
 	const togglePlayPause = () => {
-		setIsPlaying(isPlaying);
 		setIsPlaying(!isPlaying);
 	};
+
+	useEffect(() => {
+		if (isPlaying) {
+			audioElement.current.play();
+		} else if (!isPlaying) {
+			audioElement.current.pause();
+		}
+	}, [isPlaying]);
 
 	return (
 		<div className="player">
 			<AlbumPic />
 			<ProgresBar isPlaying={isPlaying} />
+			<audio ref={audioElement} src={typebeat} />
 			<Controls togglePlayPause={togglePlayPause} isPlaying={isPlaying} />
 		</div>
 	);
--- player/src/components/ProgresBar/ProgresBar.tsx
+++ player/src/components/ProgresBar/ProgresBar.tsx
@@ -1,70 +1,31 @@
 import { useEffect, useRef, useState } from "react";
-import typebeat from "../../songs/typeBeat.mp3";
-import lofi from "../../songs/88 Keys.mp3";
 import "./ProgresBar.css";
 
 export function ProgresBar(props: any) {
-	const audioElement = useRef(audioContext);
-	const [song, setSong] = useState(typebeat);
-	const [length, setLength] = useState<string>();
-	const [currentTime, setCurrentTime] = useState("0:00");
-	const [now, setNow] = useState(0);
-
-	const [audioContext, setAudioContext] = useState(null);
-
-	useEffect(() => {
-		const newAudioContext = new AudioContext();
-		setAudioContext(newAudioContext);
-	}, []);
-
-	const getLength = (duration: number) => {
-		const min = Math.floor(duration / 60);
-		const sec = Math.floor(duration - min * 60);
-		const length = min + ":" + sec;
-		setLength(length);
-	};
-
-	const updateCurrentTime = () => {
-		const minutes = Math.floor(audioElement.current!.currentTime / 60);
-		const seconds = audioElement.current!.currentTime % 60;
-		const formattedTime = `${minutes}:${seconds
-			.toFixed(0)
-			.toString()
-			.padStart(2, "0")}`;
-		setCurrentTime(formattedTime);
-		setNow(audioElement.current!.currentTime);
-	};
-
-	// const playSound = async () => {
-	// 	const audio = new Audio();
-	// 	audio.src = typebeat;
-	// 	const source = audioContext.createMediaElementSource(audio);
-	// 	source.connect(audioContext.destination);
-	// 	audio.play();
+	// const getLength = (duration: number) => {
+	// 	const min = Math.floor(duration / 60);
+	// 	const sec = Math.floor(duration - min * 60);
+	// 	const length = min + ":" + sec;
+	// 	setLength(length);
 	// };
 
-	useEffect(() => {
-		var audio = new Audio();
-		audio.src = typebeat;
-		const source = audioContext.createMediaElementSource(audio);
-		source.connect(audioContext.destination);
-		audio.play();
-	}, [props.isPlaying]);
+	// const updateCurrentTime = () => {
+	// 	const minutes = Math.floor(audioElement.current!.currentTime / 60);
+	// 	const seconds = audioElement.current!.currentTime % 60;
+	// 	const formattedTime = `${minutes}:${seconds
+	// 		.toFixed(0)
+	// 		.toString()
+	// 		.padStart(2, "0")}`;
+	// 	setCurrentTime(formattedTime);
+	// 	setNow(audioElement.current!.currentTime);
+	// };
 
 	return (
 		<div className="progresBar">
-			<input
-				type="range"
-				className="volumeControl"
-				max={length}
-				value={now}
-				onChange={(e) => {
-					console.log(e.target.value);
-				}}
-			/>
+			<input type="range" className="volumeControl" />
 			<div className="length">
-				<span className="currentTime">{currentTime}</span>
-				<span className="length">{length}</span>
+				<span className="currentTime"></span>
+				<span className="length"></span>
 			</div>
 		</div>
 	);
